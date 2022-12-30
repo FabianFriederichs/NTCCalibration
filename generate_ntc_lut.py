863829ca53bba5c3007e20941469ae9185ecf8c4
Fabian Friederichs <fabian.friederichs@gmail.com>
2022-12-30 22:50:29 +0100

    nope

--- generate_ntc_lut.py
+++ generate_ntc_lut.py
@@ -16,10 +16,11 @@ def main(argv):
     parser = argparse.ArgumentParser(description="Generate lookup tables for NTC thermistors using measured temperature and ADC values")
     parser.add_argument("--input_file", required = True, type=argparse.FileType('r'), help="Input file containing measured temperature and ADC values (2-column CSV)")
     parser.add_argument("--output_file", required = False, type=str, help="Output file containing lookup table (2-column CSV)")
-    parser.add_argument("--source_adc_res", required = True, type=int, help="Source ADC resolution (bits)")
-    parser.add_argument("--target_adc_res", required = True, type=int, help="Target ADC resolution (bits)")
-    parser.add_argument("--reference_voltage", required = True, type=float, help="Reference voltage (V)")
-    parser.add_argument("--pull_up_resistance", required = True, type=float, help="Pull-up resistance (ohms)")
+    parser.add_argument("--resistance_mode", help="Only generate resistance / temperature instead of ADC / temperature LUTs", action="store_true")
+    parser.add_argument("--source_adc_res", required = False, type=int, help="Source ADC resolution (bits)")
+    parser.add_argument("--target_adc_res", required = False, type=int, help="Target ADC resolution (bits)")
+    parser.add_argument("--reference_voltage", required = False, type=float, help="Reference voltage (V)")
+    parser.add_argument("--pull_up_resistance", required = False, type=float, help="Pull-up resistance (ohms)")
     parser.add_argument("--sample_temp_start", required = False, type = float, help="Sample temperature start (Celsius)", default = 0.0)
     parser.add_argument("--sample_temp_end", required = False, type = float, help="Sample temperature end (Celsius)", default = 350.0)
     parser.add_argument("--sample_temp_step", required = False, type = float, help="Sample temperature step (Celsius)", default = 10.0)
@@ -35,10 +36,7 @@ def main(argv):
         reader = csv.reader(f)
         input_data = list(reader)
     measured_temps = [float(row[0]) for row in input_data]
-    adc_values = [float(row[1]) for row in input_data]
-
-    # calculate resistances
-    measured_resistances = ntc_lut.adc_to_resistance(adc_values, args.source_adc_res, args.reference_voltage, args.pull_up_resistance)
+    target_values = [float(row[1]) for row in input_data]
 
     # get steinhart-hart powers
     powers = [int(p) for p in args.steinhart_hart_powers.split(",")]
@@ -50,19 +48,30 @@ def main(argv):
         sample_temps = measured_temps
 
     # fit lut
-    lut, sh_coeffs = ntc_lut.fit_lut(
-        measured_temps,
-        sample_temps,
-        adc_values,
-        args.source_adc_res,
-        args.target_adc_res,
-        args.reference_voltage,
-        args.pull_up_resistance,
-        powers,
-        not args.temp_in_kelvin,
-        args.extrapolation_max_iterations,
-        args.extrapolation_tolerance
-    )
+    if args.resistance_mode:
+        lut, sh_coeffs = ntc_lut.fit_resistance_lut(
+            measured_temps,
+            sample_temps,
+            target_values,
+            powers,
+            not args.temp_in_kelvin,
+            args.extrapolation_max_iterations,
+            args.extrapolation_tolerance
+        )
+    else:
+        lut, sh_coeffs = ntc_lut.fit_adc_lut(
+            measured_temps,
+            sample_temps,
+            target_values,
+            args.source_adc_res,
+            args.target_adc_res,
+            args.reference_voltage,
+            args.pull_up_resistance,
+            powers,
+            not args.temp_in_kelvin,
+            args.extrapolation_max_iterations,
+            args.extrapolation_tolerance
+        )
 
     # print steinhart-hart coefficients
     print("Steinhart-Hart coefficients:")
@@ -74,16 +83,23 @@ def main(argv):
 
     # plot fitted model and data points if plotting is enabled.
     # two plots, one with ADC vs. temperature and one with resistance vs. temperature
-    if not args.noplot:        
-        fig = plt.figure()
-        ax1, ax2 = fig.subplots(2, 1)
+    if not args.noplot:
+        # calculate resistances
+        if args.resistance_mode:
+            measured_resistances = target_values
+        else:
+            measured_resistances = ntc_lut.adc_to_resistance(target_values, args.source_adc_res, args.reference_voltage, args.pull_up_resistance)
 
-        # calculate points for fitted model
-        model_temps = np.arange(min(measured_temps), max(measured_temps), c_plot_resolution)
-        model_resistances = np.array([sh.inverse_steinhart_hart(model_temps[i], sh_coeffs, powers, 1.0, not args.temp_in_kelvin, 1e-6, args.extrapolation_max_iterations, args.extrapolation_tolerance) for i in range(len(model_temps))])
-        model_adc_values = ntc_lut.resistance_to_adc(model_resistances, args.source_adc_res, args.reference_voltage, args.pull_up_resistance)
+        fig = plt.figure()
+        if args.resistance_mode:
+            ax1 = fig.subplots(1, 1)
+        else:
+            ax1, ax2 = fig.subplots(2, 1)
 
         # resistance vs. temperature
+        model_temps = np.arange(min(measured_temps), max(measured_temps), c_plot_resolution)
+        model_resistances = np.array([sh.inverse_steinhart_hart(model_temps[i], sh_coeffs, powers, 1.0, not args.temp_in_kelvin, 1e-6, args.extrapolation_max_iterations, args.extrapolation_tolerance) for i in range(len(model_temps))])
+
         ax1.set_title("Resistance vs. Temperature")
         ax1.scatter(measured_resistances, measured_temps, label="Measured data points")
         ax1.plot(model_resistances, model_temps, label="Fitted model")
@@ -91,13 +107,16 @@ def main(argv):
         ax1.set_xlabel("Resistance (ohms)")
         ax1.legend()
 
-        # adc vs. temperature
-        ax2.set_title(f"ADC value (0-{2**args.source_adc_res - 1}) vs. Temperature")
-        ax2.scatter(adc_values, measured_temps, label="Measured data points")
-        ax2.plot(model_adc_values, model_temps, label="Fitted model")
-        ax2.set_ylabel(f"Temperature ({'°C' if not args.temp_in_kelvin else 'K'})")
-        ax2.set_xlabel(f"ADC value (0-{2**args.source_adc_res - 1})")
-        ax2.legend()
+        if not args.resistance_mode:
+            # adc vs. temperature
+            model_adc_values = ntc_lut.resistance_to_adc(model_resistances, args.source_adc_res, args.reference_voltage, args.pull_up_resistance)
+
+            ax2.set_title(f"ADC value (0-{2**args.source_adc_res - 1}) vs. Temperature")
+            ax2.scatter(target_values, measured_temps, label="Measured data points")
+            ax2.plot(model_adc_values, model_temps, label="Fitted model")
+            ax2.set_ylabel(f"Temperature ({'°C' if not args.temp_in_kelvin else 'K'})")
+            ax2.set_xlabel(f"ADC value (0-{2**args.source_adc_res - 1})")
+            ax2.legend()
 
         fig.tight_layout()
         plt.show(block = True)
--- ntc_lut.py
+++ ntc_lut.py
@@ -38,7 +38,7 @@ def resistance_to_adc(
     return adc_values
 
 # given a set of steinhart-hart coeffients, return a LUT of ADC value to temperature pairs
-def steinhart_hart_to_lut(
+def steinhart_hart_to_adc_lut(
     sample_temps,
     adc_resolution,
     reference_voltage,
@@ -66,8 +66,26 @@ def steinhart_hart_to_lut(
     # return the LUT
     return np.array([sample_temps, sample_adc_values]).T
 
+# given a set of steinhart-hart coeffients, return a LUT of resistance to temperature pairs
+def steinhart_hart_to_resistance_lut(
+    sample_temps,
+    coefficients,
+    terms = [0, 1, 3],
+    temp_in_celsius = True,
+    max_iterations = 1000,
+    tolerance = 1e-6
+):
+    # assert that the number of coefficients is at least 1
+    assert len(coefficients) >= 1, "Number of coefficients must be at least 1"
+    # assert that the number of sample temperatures is at least 1
+    assert len(sample_temps) > 0, "Number of sample temperatures must be at least 1"
+    # get resistance values for each sample temperature
+    sample_resistances = np.array([sh.inverse_steinhart_hart(sample_temps[i], coefficients, terms, 1.0, True, 1e-6, max_iterations, tolerance) for i in range(len(sample_temps))])    
+    # return the LUT
+    return np.array([sample_temps, sample_resistances]).T
+
 # given raw ADC values and measured temperatures, return a new LUT and the fitted steinhart-hart coefficients
-def fit_lut(
+def fit_adc_lut(
     measured_temps,
     sample_temps,
     adc_values,
@@ -102,11 +120,52 @@ def fit_lut(
     sh_coeffs = sh.fit_steinhart_hart(measured_temps, resistances, steinhart_hart_powers, temp_in_celsius)
 
     # convert steinhart-hart model to LUT
-    lut = steinhart_hart_to_lut(
+    lut = steinhart_hart_to_adc_lut(
         sample_temps,
         target_adc_resolution,
         reference_voltage,
         pull_up_resistance,
+        sh_coeffs,
+        steinhart_hart_powers,
+        temp_in_celsius,
+        extrapolation_max_iterations,
+        extrapolation_tolerance
+    )
+
+    # return the LUT and the fitted steinhart-hart coefficients
+    return lut, sh_coeffs
+
+# given raw resistance values and measured temperatures, return a new LUT and the fitted steinhart-hart coefficients
+def fit_resistance_lut(
+    measured_temps,
+    sample_temps,
+    resistance_values,
+    steinhart_hart_powers = [0, 1, 3],
+    temp_in_celsius = True,
+    extrapolation_max_iterations = 1000,
+    extrapolation_tolerance = 1e-6
+):
+    # assert that the number of measured temperatures is at least 1
+    assert len(measured_temps) > 0, "Number of measured temperatures must be at least 1"
+    # assert that number of measured temps and ADC values are equal
+    assert len(measured_temps) == len(resistance_values), "Number of measured temperatures and ADC values must be equal"
+    # assert that steinhart-hart powers is at least 1
+    assert len(steinhart_hart_powers) > 0, "Number of steinhart-hart powers must be at least 1"
+
+    # convert temps and ADC values to numpy arrays
+    if type(measured_temps) is not np.ndarray:
+        measured_temps = np.array(measured_temps)
+    if type(sample_temps) is not np.ndarray:
+        sample_temps = np.array(sample_temps)
+    if type(resistance_values) is not np.ndarray:
+        resistance_values = np.array(resistance_values)
+
+    # fit steinhart-hart coefficients
+    sh_coeffs = sh.fit_steinhart_hart(measured_temps, resistance_values, steinhart_hart_powers, temp_in_celsius)
+
+    # convert steinhart-hart model to LUT
+    lut = steinhart_hart_to_resistance_lut(
+        sample_temps,
         sh_coeffs,
         steinhart_hart_powers,
         temp_in_celsius,
